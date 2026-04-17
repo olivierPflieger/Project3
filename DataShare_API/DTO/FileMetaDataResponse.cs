44a0abbaf1862c12c2d892e41ed8425015e693b0
olivier Pflieger <keryss@gmail.com>
2026-04-17 08:33:07 +0200

    feat: Ajout de la durée d'expiration saisie en retour d'appel API

--- DataShare_API/DTO/FileMetaDataResponse.cs
+++ DataShare_API/DTO/FileMetaDataResponse.cs
@@ -7,6 +7,7 @@
         public string Extension { get; set; } = string.Empty;
         public string FileSize { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.MinValue;
+        public int ExpirationDays { get; set; } = 7;
         public bool IsExpired { get; set; } = false; 
         public bool IsProtected { get; set; } = false;
         public int RemainingDays { get; set; } = 0;
